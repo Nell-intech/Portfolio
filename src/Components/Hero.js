e52f145d5edd1b73ebb321dd653112ae14ea1cd9
Jeneil Henry <shenry.jeneil@gmail.com>
2023-04-13 17:21:49 -0400

    added navbar

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,6 +1,6 @@
 
 export default function Hero(){
     return(
-        <div className="black height400 white "> This is a space for my hero</div>
+        <div className="hero"> This is a space for my hero</div>
     );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,44 @@
-// import { Link, NavLink } from 'react-router-dom'
-
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-export default function Navbar1() {
-  return (
-    <>
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">Jeneil Henry</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto">
+//FONT AWESOME IMPORTS
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-          <Nav.Link className="navLink" href="/">Home</Nav.Link>
-                <Nav.Link className="navLink" href="/projects">Projects</Nav.Link>
-               <Nav.Link className="navLink" href="/contact">Contact</Nav.Link>
-           
+export default function navBar1() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container className="container">
+        <Navbar.Brand href="/">Jeneil Henry</Navbar.Brand>
+        {/* <FontAwesomeIcon icon={faBars} style={{color: "#0564646",}} /> */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link className="navLink" href="/">Home</Nav.Link>
+            <Nav.Link className="navLink" href="/projects">Featured Projects</Nav.Link>
+            <Nav.Link className="navLink" href="/contact">Contact</Nav.Link>
+            <NavDropdown title="Social Media Links" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">LinkedIn</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Instagram
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Facebook</NavDropdown.Item>
+              {/* <NavDropdown.Divider /> */}
+              {/* <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item> */}
+            </NavDropdown>
           </Nav>
-          
         </Navbar.Collapse>
       </Container>
     </Navbar>
-</>
   );
 }
 
+
+
+//  <Nav.Link classNameName="navLink" href="/">Home</Nav.Link>
+//                 <Nav.Link classNameName="navLink" href="/projects">Projects</Nav.Link>
+//                <Nav.Link classNameName="navLink" href="/contact">Contact</Nav.Link>
