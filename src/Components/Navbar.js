46e3cd40464f761d6e4bb559e3732b529548effb
Jeneil Henry <shenry.jeneil@gmail.com>
2023-04-22 14:57:09 -0400

    added changes to nav links for testing

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from 'react-router-dom'
 
 //FONT AWESOME IMPORTS
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -16,7 +17,7 @@ export default function navBar1() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link className="navLink" href="/">Home</Nav.Link>
+            <NavLink className="navLink" href="/">Home</NavLink>
             <Nav.Link className="navLink" href="/about">About</Nav.Link>
             <Nav.Link className="navLink" href="/projects">Projects</Nav.Link> 
             {/* include current project in this section */}
